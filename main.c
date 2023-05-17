ac8a2f60ea0e112274fb7937d5aa4195095e6354
Hugo Guiomar <82777546+HugoSilva4@users.noreply.github.com>
2023-05-17 12:22:46 +0200

    Atmel Studio fix

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * IOT.c
+ *
+ * Created: 5/17/2023 11:32:38 AM
+ * Author : guiom
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
