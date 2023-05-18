d0f8da1b9af6bec0c6fa9d7e566bf98755351542
Hugo Guiomar <82777546+HugoSilva4@users.noreply.github.com>
2023-05-18 19:22:01 +0200

    humidity reader

--- include/humidity.h
+++ include/humidity.h
@@ -1,3 +1,16 @@
 #pragma once
 
-int measureHumidity();
+#include <stdio.h>
+#include <stdint.h>
+
+//FreeRTOS
+#include <ATMEGA_FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+#include <event_groups.h>
+
+#include <hih8120.h>
+
+void initialiseHumidity();
+void measureHumidity();
+void humidityTask(void* parameter);
--- src/humidity.c
+++ src/humidity.c
@@ -2,34 +2,90 @@
 #include <avr/delay.h>
 #include <hih8120.h>
 
-int measureHumidity() {
-    if ( HIH8120_OK == hih8120_initialise() )
-    {
-        // Driver initialised OK
-        // Always check what hih8120_initialise() returns
-    }
+static TaskHandle_t _HumiditySensorTaskHandle;
 
+//private fields
+static SemaphoreHandle_t _xPrintfSemaphore;
+static EventGroupHandle_t _eventGroupHandleMeasure;
+static EventGroupHandle_t _eventGroupHandleNewData;
+static float _lastMeasurementHumidity;
+
+// Delay constants
+#define DELAY_WAKEUP 15
+#define DELAY_MEASUREMENT 20
+
+/**
+ * Initializes the humidity sensor and checks if the initialization was successful.
+ * Prints a message indicating the status of the driver.
+ */
+void initialiseHumidity()
+{
+    // Call the humidity sensor initialization function
+    hih8120_driverReturnCode_t returncode = hih8120_initialise();
+
+    // Check if the initialization was successful
+    if (HIH8120_OK == returncode)
+    {
+        printf("Humidity driver has been initialized\n");
+    }
+    else
+    {
+        printf("Driver not working\n");
+    }
+}
+
+
+
+void measureHumidity()
+{
     float humidity = 0.0;
-    //float temperature = 0.0;
+    // Wait until the measure event is triggered
+    xEventGroupWaitBits(_eventGroupHandleMeasure,
+                        pdTRUE,
+                        pdTRUE,
+                        portMAX_DELAY);
 
-    if ( HIH8120_OK != hih8120_wakeup() )
+    hih8120_driverReturnCode_t result;
+
+    // Wakes up the sensor from standby
+    result = hih8120_wakeup();
+    // After the sensor wakes up, it requires at least 100 ms to start measuring
+    vTaskDelay(DELAY_WAKEUP);
+    result = hih8120_measure();
+    // Requires a delay to fetch data from the sensor
+    vTaskDelay(DELAY_MEASUREMENT);
+
+    if (HIH8120_OK != result)
     {
-        // Something went wrong
-        // Investigate the return code further
-        return -1;
+        // Set trials to get measurements from the sensor
+        int count = 15;
+        while ((HIH8120_TWI_BUSY == result) && count > 0)
+        {
+            result = hih8120_measure();
+            vTaskDelay(DELAY_MEASUREMENT);
+            count--;
+        }
     }
-    _delay_ms(100);
 
-    if ( HIH8120_OK !=  hih8120_measure() )
+    if (result == HIH8120_OK)
     {
-        // Something went wrong
-        // Investigate the return code further
-        return -1;
+        _lastMeasurementHumidity = hih8120_getHumidity();
+        xEventGroupSetBits(_eventGroupHandleNewData);
     }
+}
 
-    _delay_ms(4);
+}
 
-    humidity = hih8120_getHumidity();
-    //temperature = hih8120_getTemperature();
-    return humidity;
+void humiditySensorTask(void* pvParameters)
+{
+for(;;)
+{
+    measureHumidity();
+}
+vTaskDelete(_HumiditySensorTaskHandle);
+}
+
+float humiditySensor_getHumidity()
+{
+	return _lastMeasurementHumidity;
 }
