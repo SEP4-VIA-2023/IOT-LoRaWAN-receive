030156ac7ffbdc46995721616051a32bc42ba813
Ondrej Klimek <klimekondrej01@gmail.com>
2023-05-24 17:24:28 +0200

    downlink implementation

--- src/loraWanHandler.c
+++ src/loraWanHandler.c
@@ -11,21 +11,33 @@
 
 #include <lora_driver.h>
 #include <status_leds.h>
+#include <stdlib.h>
 
 #include "co2.h"
 #include "humidity_and_temperature.h"
+#include "servo.h"
 
 // Parameters for OTAA join - You have got these in a mail from IHA
 #define LORA_appEUI "2BBE8F09765BBF4B"
 #define LORA_appKEY "5F83717BC67B4646E3F00DC5EC3417DC"
 
 void lora_handler_task( void *pvParameters );
+void lora_handler_downlink_task( void *pvParameters );
 
 static lora_driver_payload_t _uplink_payload;
+static lora_driver_payload_t _downlink_payload;
+
+MessageBufferHandle_t downlink_message_buffer;
 
 void lora_handler_initialise(UBaseType_t lora_handler_task_priority)
 {
-	xTaskCreate(
+    // message buffer for downlink messages
+    downlink_message_buffer = xMessageBufferCreate(sizeof(lora_driver_payload_t));
+
+    // Initialise the LoRaWAN driver without down-link buffer
+    lora_driver_initialise(1, downlink_message_buffer);
+
+    xTaskCreate(
 	lora_handler_task
 	,  "LRHand"  // A name just for humans
 	,  configMINIMAL_STACK_SIZE+200  // This stack size can be checked & adjusted by reading the Stack Highwater
@@ -150,4 +162,60 @@ void lora_handler_task( void *pvParameters )
 		status_leds_shortPuls(led_ST4);  
 		printf("Upload Message >%s<\n", lora_driver_mapReturnCodeToText(lora_driver_sendUploadMessage(false, &_uplink_payload)));
 	}
+}
+
+/*-----------------------------------------------------------*/
+void lora_handler_downlink_task (void *pvParameters)
+{
+    // message buffer to receive downlink messages in
+    xMessageBufferReceive(downlink_message_buffer, &_downlink_payload, sizeof(_downlink_payload), portMAX_DELAY);
+    // for debugging purposes
+    printf("DOWN LINK: from port: %d with %d bytes received!", _downlink_payload.portNo, _downlink_payload.len);
+
+    // check we receive the correct size of the payload
+    if (_downlink_payload.len == 12)
+    {
+        // pointers for each variable of the payload
+        uint16_t *minCO2_ptr;
+        uint16_t *maxCO2_ptr;
+        uint16_t *minHum_ptr;
+        uint16_t *maxHum_ptr;
+        int16_t *minTemp_ptr;
+        int16_t *maxTemp_ptr;
+
+        // allocate memory for the values
+        minCO2_ptr = malloc(sizeof(uint16_t));
+        maxCO2_ptr = malloc(sizeof(uint16_t));
+        minHum_ptr = malloc(sizeof(uint16_t));
+        maxHum_ptr = malloc(sizeof(uint16_t));
+        minTemp_ptr = malloc(sizeof(uint16_t));
+        maxTemp_ptr = malloc(sizeof(uint16_t));
+
+        // decode the payload into the variables through the pointers
+        *minCO2_ptr = (_downlink_payload.bytes[0] << 8) | _downlink_payload.bytes[1];
+        *maxCO2_ptr = (_downlink_payload.bytes[2] << 8) | _downlink_payload.bytes[3];
+        *minHum_ptr = (_downlink_payload.bytes[4] << 8) | _downlink_payload.bytes[5];
+        *maxHum_ptr = (_downlink_payload.bytes[6] << 8) | _downlink_payload.bytes[7];
+        *minTemp_ptr = (_downlink_payload.bytes[8] << 8) | _downlink_payload.bytes[9];
+        *maxTemp_ptr = (_downlink_payload.bytes[10] << 8) | _downlink_payload.bytes[11];
+
+        // use the values to update the servo configuration
+        updateConfiguration(*minCO2_ptr, *maxCO2_ptr, *minTemp_ptr, *maxTemp_ptr, *minHum_ptr, *maxHum_ptr);
+
+        // free the memory
+        free(minCO2_ptr);
+        free(maxCO2_ptr);
+        free(minHum_ptr);
+        free(maxHum_ptr);
+        free(minTemp_ptr);
+        free(maxTemp_ptr);
+
+        // set pointers to NULL
+        minCO2_ptr = NULL;
+        maxCO2_ptr = NULL;
+        minHum_ptr = NULL;
+        maxHum_ptr = NULL;
+        minTemp_ptr = NULL;
+        maxTemp_ptr = NULL;
+    }
 }
