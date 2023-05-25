1d8750b38b5bee3df38d9534dc30f047cfcf6dda
Ondrej Klimek <klimekondrej01@gmail.com>
2023-05-25 09:46:07 +0200

    downlink handling - NON-FUNCTIONAL for now

--- src/loraWanHandler.c
+++ src/loraWanHandler.c
@@ -22,7 +22,6 @@
 #define LORA_appKEY "5F83717BC67B4646E3F00DC5EC3417DC"
 
 void lora_handler_task( void *pvParameters );
-void lora_handler_downlink_task( void *pvParameters );
 
 static lora_driver_payload_t _uplink_payload;
 static lora_driver_payload_t _downlink_payload;
@@ -44,13 +43,15 @@ void lora_handler_initialise(UBaseType_t lora_handler_task_priority)
 	,  NULL
 	,  lora_handler_task_priority  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
 	,  NULL );
+
+
 }
 
 static void _lora_setup(void)
 {
 	char _out_buf[20];
 	lora_driver_returnCode_t rc;
-	status_leds_slowBlink(led_ST2); // OPTIONAL: Led the green led blink slowly while we are setting up LoRa
+	status_leds_slowBlink(led_ST3); // Led the green led blink slowly while we are setting up LoRa
 
 	// Factory reset the transceiver
 	printf("FactoryReset >%s<\n", lora_driver_mapReturnCodeToText(lora_driver_rn2483FactoryReset()));
@@ -87,12 +88,13 @@ static void _lora_setup(void)
 		if ( rc != LORA_ACCEPTED)
 		{
 			// Make the red led pulse to tell something went wrong
-			status_leds_longPuls(led_ST1); // OPTIONAL
+			status_leds_fastBlink(led_ST1);
 			// Wait 5 sec and lets try again
 			vTaskDelay(pdMS_TO_TICKS(5000UL));
 		}
 		else
 		{
+            status_leds_longPuls(led_ST3);
 			break;
 		}
 	} while (--maxJoinTriesLeft);
@@ -101,15 +103,15 @@ static void _lora_setup(void)
 	{
 		// Connected to LoRaWAN :-)
 		// Make the green led steady
-		status_leds_ledOn(led_ST2); // OPTIONAL
+		status_leds_ledOn(led_ST2);
 	}
 	else
 	{
 		// Something went wrong
 		// Turn off the green led
-		status_leds_ledOff(led_ST2); // OPTIONAL
+		status_leds_ledOff(led_ST2);
 		// Make the red led blink fast to tell something went wrong
-		status_leds_fastBlink(led_ST1); // OPTIONAL
+		status_leds_fastBlink(led_ST1);
 
 		// Lets stay here
 		while (1)
@@ -133,8 +135,13 @@ void lora_handler_task( void *pvParameters )
 
 	_lora_setup();
 
+    lora_driver_returnCode_t rc;
+
 	_uplink_payload.len = 7;
 	_uplink_payload.portNo = 2;
+
+    _downlink_payload.len = 12;
+    _downlink_payload.portNo = 1;
 
 	TickType_t xLastWakeTime;
 	const TickType_t xFrequency = pdMS_TO_TICKS(300000UL); // Upload message every 5 minutes (300000 ms)
@@ -142,6 +149,9 @@ void lora_handler_task( void *pvParameters )
 	
 	for(;;)
 	{
+
+        // ********** UPLINK (TX) **********
+
 		xTaskDelayUntil( &xLastWakeTime, xFrequency );
 
 		// current data payload
@@ -159,63 +169,100 @@ void lora_handler_task( void *pvParameters )
 		_uplink_payload.bytes[5] = temp & 0xFF; 		// least significant byte
 		_uplink_payload.bytes[6] = servstatus & 0xFF; 	// 8bit boolean
 
-		status_leds_shortPuls(led_ST4);  
-		printf("Upload Message >%s<\n", lora_driver_mapReturnCodeToText(lora_driver_sendUploadMessage(false, &_uplink_payload)));
+		status_leds_ledOn(led_ST4);
+		/*
+		 * printf("Upload Message >%s<\n",
+               lora_driver_mapReturnCodeToText(lora_driver_sendUploadMessage(false, &_uplink_payload)));
+        */
+        //rc = lora_driver_sendUploadMessage(false, &_uplink_payload);
+        if ((rc = lora_driver_sendUploadMessage(false, &_uplink_payload)) == LORA_MAC_TX_OK)
+        {
+            status_leds_longPuls(led_ST4);
+            printf("Upload Message OK - No Message Received!!\n");
+        }
+        else
+        {
+            if (rc == LORA_MAC_RX)
+            {
+                // ********** DOWNLINK MESSAGE HANDLING (RX) **********
+
+                status_leds_fastBlink(led_ST4);
+                // message buffer to receive downlink messages in
+                xMessageBufferReceive(downlink_message_buffer, &_downlink_payload, sizeof(_downlink_payload), portMAX_DELAY);
+                // for debugging purposes
+                printf("Upload Message OK - \n\tDOWN LINK: from port: %d with %d bytes received!", _downlink_payload.portNo, _downlink_payload.len);
+
+                // check we receive the correct size of the payload
+                if (_downlink_payload.len == 12)
+                {
+                    // pointers for each variable of the payload
+                    uint16_t *minCO2_ptr;
+                    uint16_t *maxCO2_ptr;
+                    uint16_t *minHum_ptr;
+                    uint16_t *maxHum_ptr;
+                    int16_t *minTemp_ptr;
+                    int16_t *maxTemp_ptr;
+
+                    // allocate memory for the values
+                    minCO2_ptr = malloc(sizeof(uint16_t));
+                    maxCO2_ptr = malloc(sizeof(uint16_t));
+                    minHum_ptr = malloc(sizeof(uint16_t));
+                    maxHum_ptr = malloc(sizeof(uint16_t));
+                    minTemp_ptr = malloc(sizeof(uint16_t));
+                    maxTemp_ptr = malloc(sizeof(uint16_t));
+
+                    // decode the payload into the variables through the pointers
+                    *minCO2_ptr = (_downlink_payload.bytes[0] << 8) | _downlink_payload.bytes[1];
+                    *maxCO2_ptr = (_downlink_payload.bytes[2] << 8) | _downlink_payload.bytes[3];
+                    *minHum_ptr = (_downlink_payload.bytes[4] << 8) | _downlink_payload.bytes[5];
+                    *maxHum_ptr = (_downlink_payload.bytes[6] << 8) | _downlink_payload.bytes[7];
+                    *minTemp_ptr = (_downlink_payload.bytes[8] << 8) | _downlink_payload.bytes[9];
+                    *maxTemp_ptr = (_downlink_payload.bytes[10] << 8) | _downlink_payload.bytes[11];
+
+                    // use the values to update the servo configuration
+                    updateConfiguration(*minCO2_ptr, *maxCO2_ptr, *minTemp_ptr, *maxTemp_ptr, *minHum_ptr, *maxHum_ptr);
+
+                    // if statement for debugging purposes
+                    if (*maxCO2_ptr == 0xFFFF)
+                    {
+                        // for debugging purposes
+                        status_leds_fastBlink(led_ST3);
+                    }
+                    else
+                    {
+                        // for debugging purposes
+                        status_leds_shortPuls(led_ST3);
+                    }
+
+                    vTaskDelay(20);
+
+                    // free the memory
+                    free(minCO2_ptr);
+                    free(maxCO2_ptr);
+                    free(minHum_ptr);
+                    free(maxHum_ptr);
+                    free(minTemp_ptr);
+                    free(maxTemp_ptr);
+
+                    // set pointers to NULL
+                    minCO2_ptr = NULL;
+                    maxCO2_ptr = NULL;
+                    minHum_ptr = NULL;
+                    maxHum_ptr = NULL;
+                    minTemp_ptr = NULL;
+                    maxTemp_ptr = NULL;
+
+                    // turn off led from debug if ()
+                    status_leds_ledOff(led_ST3);
+                }
+            }
+            else
+            {
+                status_leds_fastBlink(led_ST1);
+                printf("Upload Message failed >%s<\n", lora_driver_mapReturnCodeToText(rc));
+            }
+        }
 	}
 }
 
 /*-----------------------------------------------------------*/
-void lora_handler_downlink_task (void *pvParameters)
-{
-    // message buffer to receive downlink messages in
-    xMessageBufferReceive(downlink_message_buffer, &_downlink_payload, sizeof(_downlink_payload), portMAX_DELAY);
-    // for debugging purposes
-    printf("DOWN LINK: from port: %d with %d bytes received!", _downlink_payload.portNo, _downlink_payload.len);
-
-    // check we receive the correct size of the payload
-    if (_downlink_payload.len == 12)
-    {
-        // pointers for each variable of the payload
-        uint16_t *minCO2_ptr;
-        uint16_t *maxCO2_ptr;
-        uint16_t *minHum_ptr;
-        uint16_t *maxHum_ptr;
-        int16_t *minTemp_ptr;
-        int16_t *maxTemp_ptr;
-
-        // allocate memory for the values
-        minCO2_ptr = malloc(sizeof(uint16_t));
-        maxCO2_ptr = malloc(sizeof(uint16_t));
-        minHum_ptr = malloc(sizeof(uint16_t));
-        maxHum_ptr = malloc(sizeof(uint16_t));
-        minTemp_ptr = malloc(sizeof(uint16_t));
-        maxTemp_ptr = malloc(sizeof(uint16_t));
-
-        // decode the payload into the variables through the pointers
-        *minCO2_ptr = (_downlink_payload.bytes[0] << 8) | _downlink_payload.bytes[1];
-        *maxCO2_ptr = (_downlink_payload.bytes[2] << 8) | _downlink_payload.bytes[3];
-        *minHum_ptr = (_downlink_payload.bytes[4] << 8) | _downlink_payload.bytes[5];
-        *maxHum_ptr = (_downlink_payload.bytes[6] << 8) | _downlink_payload.bytes[7];
-        *minTemp_ptr = (_downlink_payload.bytes[8] << 8) | _downlink_payload.bytes[9];
-        *maxTemp_ptr = (_downlink_payload.bytes[10] << 8) | _downlink_payload.bytes[11];
-
-        // use the values to update the servo configuration
-        updateConfiguration(*minCO2_ptr, *maxCO2_ptr, *minTemp_ptr, *maxTemp_ptr, *minHum_ptr, *maxHum_ptr);
-
-        // free the memory
-        free(minCO2_ptr);
-        free(maxCO2_ptr);
-        free(minHum_ptr);
-        free(maxHum_ptr);
-        free(minTemp_ptr);
-        free(maxTemp_ptr);
-
-        // set pointers to NULL
-        minCO2_ptr = NULL;
-        maxCO2_ptr = NULL;
-        minHum_ptr = NULL;
-        maxHum_ptr = NULL;
-        minTemp_ptr = NULL;
-        maxTemp_ptr = NULL;
-    }
-}
