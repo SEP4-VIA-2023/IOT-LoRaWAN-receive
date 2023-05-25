1206bb1316b6dfb0ad0e72eb98b1d67065b113bb
Emilia Nistor <emilia.georgiana24@gmail.com>
2023-05-25 15:37:31 +0200

    co2 test

---
+++ test/co2test.cpp
@@ -0,0 +1,39 @@
+#include "gtest/gtest.h"
+#include "FreeRTOS_defs/fff.h"
+#include "FreeRTOS_defs/FreeRTOS_FFF_MocksDeclaration.h"
+
+// Include interfaces and define global variables
+// defined by the production code
+extern "C" {
+#include <mh_z19.h>
+#include "co2.h"
+}
+
+// Declare and define fakes using FFF
+FAKE_VALUE_FUNC(mh_z19_returnCode_t, mh_z19_takeMeassuring);
+FAKE_VALUE_FUNC(uint16_t, readCO2);
+
+// Test fixture for CO2 functions
+class CO2Test : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // Reset the fakes and clear the history before each test
+        RESET_FAKE(mh_z19_takeMeassuring);
+        RESET_FAKE(readCO2);
+        FFF_RESET_HISTORY();
+    }
+};
+
+
+// Test the readCO2() function
+TEST_F(CO2Test, ReadCO2) {
+    // Set up test variables
+    uint16_t CO2;
+
+    // Call the function under test
+    CO2 = readCO2();
+
+    // Check the result
+    EXPECT_EQ(0, CO2);
+}
+
