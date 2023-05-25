3f45f409b6126aa1e66e982a35c02bf7507d2fd8
Ondrej Klimek <klimekondrej01@gmail.com>
2023-05-25 12:16:33 +0200

    live issue with downlink

--- src/loraWanHandler.c
+++ src/loraWanHandler.c
@@ -16,6 +16,7 @@
 #include "co2.h"
 #include "humidity_and_temperature.h"
 #include "servo.h"
+#include "display_7seg.h"
 
 // Parameters for OTAA join - You have got these in a mail from IHA
 #define LORA_appEUI "2BBE8F09765BBF4B"
@@ -51,32 +52,40 @@ static void _lora_setup(void)
 {
 	char _out_buf[20];
 	lora_driver_returnCode_t rc;
-	status_leds_slowBlink(led_ST3); // Led the green led blink slowly while we are setting up LoRa
+	status_leds_slowBlink(led_ST3); // Led the yellow led blink slowly while we are setting up LoRa
 
 	// Factory reset the transceiver
-	printf("FactoryReset >%s<\n", lora_driver_mapReturnCodeToText(lora_driver_rn2483FactoryReset()));
+	printf("FactoryReset >%s<\n",
+           lora_driver_mapReturnCodeToText(lora_driver_rn2483FactoryReset()));
 	
 	// Configure to EU868 LoRaWAN standards
-	printf("Configure to EU868 >%s<\n", lora_driver_mapReturnCodeToText(lora_driver_configureToEu868()));
+	printf("Configure to EU868 >%s<\n",
+           lora_driver_mapReturnCodeToText(lora_driver_configureToEu868()));
 
 	// Get the transceivers HW EUI
 	rc = lora_driver_getRn2483Hweui(_out_buf);
-	printf("Get HWEUI >%s<: %s\n",lora_driver_mapReturnCodeToText(rc), _out_buf);
+	printf("Get HWEUI >%s<: %s\n",
+           lora_driver_mapReturnCodeToText(rc), _out_buf);
 
 	// Set the HWEUI as DevEUI in the LoRaWAN software stack in the transceiver
-	printf("Set DevEUI: %s >%s<\n", _out_buf, lora_driver_mapReturnCodeToText(lora_driver_setDeviceIdentifier(_out_buf)));
+	printf("Set DevEUI: %s >%s<\n",
+           _out_buf, lora_driver_mapReturnCodeToText(lora_driver_setDeviceIdentifier(_out_buf)));
 
 	// Set Over The Air Activation parameters to be ready to join the LoRaWAN
-	printf("Set OTAA Identity appEUI:%s appKEY:%s devEUI:%s >%s<\n", LORA_appEUI, LORA_appKEY, _out_buf, lora_driver_mapReturnCodeToText(lora_driver_setOtaaIdentity(LORA_appEUI,LORA_appKEY,_out_buf)));
+	printf("Set OTAA Identity appEUI:%s appKEY:%s devEUI:%s >%s<\n",
+           LORA_appEUI, LORA_appKEY, _out_buf, lora_driver_mapReturnCodeToText(lora_driver_setOtaaIdentity(LORA_appEUI,LORA_appKEY,_out_buf)));
 
 	// Save all the MAC settings in the transceiver
-	printf("Save mac >%s<\n",lora_driver_mapReturnCodeToText(lora_driver_saveMac()));
+	printf("Save mac >%s<\n",
+           lora_driver_mapReturnCodeToText(lora_driver_saveMac()));
 
 	// Enable Adaptive Data Rate
-	printf("Set Adaptive Data Rate: ON >%s<\n", lora_driver_mapReturnCodeToText(lora_driver_setAdaptiveDataRate(LORA_ON)));
+	printf("Set Adaptive Data Rate: ON >%s<\n",
+           lora_driver_mapReturnCodeToText(lora_driver_setAdaptiveDataRate(LORA_ON)));
 
 	// Set receiver window1 delay to 500 ms - this is needed if down-link messages will be used
-	printf("Set Receiver Delay: %d ms >%s<\n", 500, lora_driver_mapReturnCodeToText(lora_driver_setReceiveDelay(500)));
+	printf("Set Receiver Delay: %d ms >%s<\n",
+           500, lora_driver_mapReturnCodeToText(lora_driver_setReceiveDelay(500)));
 
 	// Join the LoRaWAN
 	uint8_t maxJoinTriesLeft = 10;
@@ -190,7 +199,13 @@ void lora_handler_task( void *pvParameters )
                 // message buffer to receive downlink messages in
                 xMessageBufferReceive(downlink_message_buffer, &_downlink_payload, sizeof(_downlink_payload), portMAX_DELAY);
                 // for debugging purposes
-                printf("Upload Message OK - \n\tDOWN LINK: from port: %d with %d bytes received!", _downlink_payload.portNo, _downlink_payload.len);
+                printf("Upload Message OK - \n\tDOWN LINK: from port: %d with %d bytes received!",
+                       _downlink_payload.portNo, _downlink_payload.len);
+
+                // display the number of bytes received for debugging purposes
+                display_7seg_displayHex("512E");
+                vTaskDelay(pdMS_TO_TICKS(3000UL));
+                display_7seg_display(_downlink_payload.len, 0);
 
                 // check we receive the correct size of the payload
                 if (_downlink_payload.len == 12)
@@ -208,8 +223,8 @@ void lora_handler_task( void *pvParameters )
                     maxCO2_ptr = malloc(sizeof(uint16_t));
                     minHum_ptr = malloc(sizeof(uint16_t));
                     maxHum_ptr = malloc(sizeof(uint16_t));
-                    minTemp_ptr = malloc(sizeof(uint16_t));
-                    maxTemp_ptr = malloc(sizeof(uint16_t));
+                    minTemp_ptr = malloc(sizeof(int16_t));
+                    maxTemp_ptr = malloc(sizeof(int16_t));
 
                     // decode the payload into the variables through the pointers
                     *minCO2_ptr = (_downlink_payload.bytes[0] << 8) | _downlink_payload.bytes[1];
@@ -220,7 +235,8 @@ void lora_handler_task( void *pvParameters )
                     *maxTemp_ptr = (_downlink_payload.bytes[10] << 8) | _downlink_payload.bytes[11];
 
                     // use the values to update the servo configuration
-                    updateConfiguration(*minCO2_ptr, *maxCO2_ptr, *minTemp_ptr, *maxTemp_ptr, *minHum_ptr, *maxHum_ptr);
+                    updateConfiguration(*minCO2_ptr, *maxCO2_ptr, *minTemp_ptr,
+                                        *maxTemp_ptr, *minHum_ptr, *maxHum_ptr);
 
                     // if statement for debugging purposes
                     if (*maxCO2_ptr == 0xFFFF)
@@ -234,7 +250,7 @@ void lora_handler_task( void *pvParameters )
                         status_leds_shortPuls(led_ST3);
                     }
 
-                    vTaskDelay(20);
+                    vTaskDelay(pdMS_TO_TICKS(20UL));
 
                     // free the memory
                     free(minCO2_ptr);
@@ -252,8 +268,10 @@ void lora_handler_task( void *pvParameters )
                     minTemp_ptr = NULL;
                     maxTemp_ptr = NULL;
 
-                    // turn off led from debug if ()
+                    // turn off LEDs from debug if () after 10 seconds
+                    vTaskDelay(pdMS_TO_TICKS(10000UL));
                     status_leds_ledOff(led_ST3);
+                    //status_leds_ledOff(led_ST4);
                 }
             }
             else
