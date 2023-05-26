94a900821c8deee64bc08de7b425226a8940526d
Andrejs <51496244+daviskyLV@users.noreply.github.com>
2023-05-26 16:45:59 +0200

    why (#2)
    * Added folders to gitignore
    
    * Deleting build files

--- cmake-build-megaatmega2560/test/IOT[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/45552/Desktop/VIA_UC/Semester4/SEP4/IOT-LoRaWAN-receive/cmake-build-megaatmega2560/test/IOT[1]_tests.cmake")
-  include("C:/Users/45552/Desktop/VIA_UC/Semester4/SEP4/IOT-LoRaWAN-receive/cmake-build-megaatmega2560/test/IOT[1]_tests.cmake")
-else()
-  add_test(IOT_NOT_BUILT IOT_NOT_BUILT)
-endif()
